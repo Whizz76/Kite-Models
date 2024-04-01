325cd3fe7ac17446bd93caa24a1c7867feddb1a9
Whizz76 <aparna.singh.che20@itbhu.ac.in>
2024-04-01 23:26:10 +0530

    committing changes

--- kiteApi/placeOrderKite.py
+++ kiteApi/placeOrderKite.py
@@ -20,28 +20,46 @@ kite = KiteConnect(api_key=api_key)
 access_token = "FaA7BWJ1xebOrSL6kfkwNtmHa7OFw8lP"
 kite.set_access_token(access_token)
 
-def place_order_kite(tradingsymbol,price,quantity,direction,order_type,product,variety,exchange,validity):
-    """
-    This function will place an order on the exchange
-    """
-    print("Placing order {} Qty: {} Direction: {}".format(tradingsymbol,quantity,direction))
-    try:
-        order_id = kite.place_order(tradingsymbol=tradingsymbol,
-                                    quantity=quantity,
-                                    transaction_type=kite.TRANSACTION_TYPE_BUY if direction == "buy" else kite.TRANSACTION_TYPE_SELL,
-                                    order_type=order_type,
-                                    product=product,
-                                    variety=variety,
-                                    price=price,
-                                    validity=validity,
-                                    exchange=exchange)
+# def place_order_kite(tradingsymbol,price,quantity,direction,order_type,product,variety,exchange,validity):
+#     """
+#     This function will place an order on the exchange
+#     """
+#     print("Placing order {} Qty: {} Direction: {}".format(tradingsymbol,quantity,direction))
+#     try:
+#         order_id = kite.place_order(tradingsymbol=tradingsymbol,
+#                                     quantity=quantity,
+#                                     transaction_type=kite.TRANSACTION_TYPE_BUY if direction == "buy" else kite.TRANSACTION_TYPE_SELL,
+#                                     order_type=order_type,
+#                                     product=product,
+#                                     variety=variety,
+#                                     price=price,
+#                                     validity=validity,
+#                                     exchange=exchange)
         
-        logging.info("Order placed. ID is: {}".format(order_id))
-        print("Order placement successful. Direction is {} OrderId is: {}".format(direction,order_id))
-        return order_id
-    except Exception as e:
-        logging.info("Order placement failed: {}".format(e.message))
+#         logging.info("Order placed. ID is: {}".format(order_id))
+#         print("Order placement successful. Direction is {} OrderId is: {}".format(direction,order_id))
+#         return order_id
+#     except Exception as e:
+#         logging.info("Order placement failed: {}".format(e.message))
 
-# get_order_id = place_order_kite("NIFTY BANK",0,1,"buy","MARKET","NRML","REGULAR","NSE","DAY")
+# get_order_id = place_order_kite("RAJMET-BE",0,1,"buy","MARKET","NRML","REGULAR","BSE","DAY")
 # print(get_order_id)
-# RAJMET-BE
+# print(kite.orders())
+# # RAJMET-BE
+
+# Place an order
+# try:
+#     order_id = kite.place_order(tradingsymbol="INFY",
+#                                 exchange=kite.EXCHANGE_BSE,
+#                                 transaction_type=kite.TRANSACTION_TYPE_BUY,
+#                                 quantity=1,
+#                                 variety=kite.VARIETY_REGULAR,
+#                                 order_type=kite.ORDER_TYPE_MARKET,
+#                                 product=kite.PRODUCT_CNC)
+
+#     logging.info("Order placed. ID is: {}".format(order_id))
+# except Exception as e:
+#     logging.info("Order placement failed: {}".format(e))
+
+# Fetch all orders
+print(kite.orders())
